ff35415f0003ef78c76fd52b5552aa13aa14a4a1
Jawad Muhammadi <jm10@Jawads-MacBook-Air.local>
2019-06-25 17:30:20 -0700

    added backend directory

--- mern-stack/src/components/create-todo.component.js
+++ mern-stack/src/components/create-todo.component.js
@@ -9,9 +9,119 @@ class CreateTodos extends Component {
       todo_priority: "",
       todo_completed: false
     };
+    this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+    this.onChangeTodoResponsilbe = this.onChangeTodoResponsilbe.bind(this);
+    this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+    this.onChangeTodoCompleted = this.onChangeTodoCompleted.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChangeTodoDescription(e) {
+    this.setState({
+      todo_description: e.target.value
+    });
+  }
+  onChangeTodoResponsilbe(e) {
+    this.setState({
+      todo_responsible: e.target.value
+    });
+  }
+  onChangeTodoPriority(e) {
+    this.setState({
+      todo_priority: e.target.value
+    });
+  }
+  onChangeTodoCompleted(e) {
+    this.setState({
+      todo_completed: e.target.value
+    });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(`Form Submitted`);
+    console.log(`Todo Description ${this.state.todo_description}`);
+    console.log(`Todo Responsible ${this.state.todo_responsible}`);
+    console.log(`Todo Priority ${this.state.todo_priority}`);
+    console.log(`Todo Completed ${this.state.todo_completed}`);
+
+    this.setState({
+      todo_description: "",
+      todo_responsible: "",
+      todo_priority: "",
+      todo_completed: false
+    });
   }
   render() {
-    return <div>Welcome to CreateTodos Component</div>;
+    return (
+      <div style={{ marginTop: 20 }}>
+        <h3>Create New Todo</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label> Description: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.todo_description}
+              onChange={this.onChangeTodoDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label> Responsible: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.todo_responsible}
+              onChange={this.onChangeTodoResponsilbe}
+            />
+          </div>
+          <div className="form-group">
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="priorityLow"
+                value="Low"
+                checked={this.state.todo_priority === "Low"}
+                onChange={this.onChangeTodoPriority}
+              />
+              <label className="form-check-label">Low</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="priorityMedium"
+                value="Medium"
+                checked={this.state.todo_priority === "Medium"}
+                onChange={this.onChangeTodoPriority}
+              />
+              <label className="form-check-label">Medium</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                name="priorityOptions"
+                type="radio"
+                id="priorityHigh"
+                value="High"
+                checked={this.state.todo_priority === "High"}
+                onChange={this.onChangeTodoPriority}
+              />
+              <label className="form-check-label">High</label>
+            </div>
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create todo"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 export default CreateTodos;
