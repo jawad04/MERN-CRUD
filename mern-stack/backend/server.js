a1a3fcf59186d9f353a8fffaa57d4098c233613e
Jawad Muhammadi <jm10@Jawads-MacBook-Air.local>
2019-06-25 18:33:00 -0700

    connected with mongodb server

---
+++ mern-stack/backend/server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Todo = require("./todo.model");
+const todoRoutes = express.Router();
+
+const PORT = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+mongoose.connect("mongodb://127.0.0.1:27017/todos", { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log(`MongoDB connection stablished sucessfully`);
+});
+todoRoutes.route("/").get(function(req, res) {
+  Todo.find(function(err, todos) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+todoRoutes.route("/:id").get(function(req, res) {
+  let id = req.params.id;
+  Todo.findById(id, function(err, res) {
+    res.json(id);
+  });
+});
+todoRoutes.route("/add").post(function(req, res) {
+  let todo = new Todo(req.body);
+  todo
+    .save()
+    .then(todo => {
+      res.status(200).json({ todo: "todo added successfully" });
+    })
+    .catch(err => {
+      res.status(400).send("adding new todo failed");
+    });
+});
+todoRoutes.route("/update/:id").post(function(req, res) {
+  Todo.findById(req.params.id, function(err, todo) {
+    if (!todo) {
+      res.status(400).send("data was not found");
+    } else {
+      todo.todo_descripton = req.body.todo_descripton;
+      todo.todo_responsible = req.body.todo_responsible;
+      todo.todo_priority = req.body.todo_priority;
+      todo.todo_completed = req.body.todo_completed;
+      todo
+        .save()
+        .then(todo => {
+          res.json("todo updated");
+        })
+        .catch(err => {
+          res.send(400).send("updated not possible");
+        });
+    }
+  });
+});
+app.use("/todos", todoRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
